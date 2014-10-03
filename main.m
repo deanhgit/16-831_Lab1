41c0e11688977c88fa05dd0b3c7784547a0b5894
De-An Huang <deanh@yell.ius.cs.cmu.edu>
2014-10-03 13:23:21 -0400

    change num of cores

--- main.m
+++ main.m
@@ -2,5 +2,5 @@ clear;
 map_file = 'wean.dat';
 data_file = './log/robotdata1.log';
 num_particles = 200;
-numCores = 4;
+numCores = 10;
 [ particles ] = particle_filter( data_file, map_file, num_particles, numCores );
